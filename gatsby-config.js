7f51bfdc6dabe2411e8a2b393b539a0908e233f2
Ste Allen <steallen@gmail.com>
2023-01-22 18:56:02 +0000

    finished gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
     title: `Rogers`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    'gatsby-plugin-mdx',
+  ],
 }
